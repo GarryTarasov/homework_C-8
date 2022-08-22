f3d14752cab94f6c51c79332042246415b28db19
Garry <igor_tarasov_84@mail.ru>
2022-08-22 12:29:25 +0500

    end homework 7 task 1-4

---
+++ Task1/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 54: Задайте двумерный массив. 
+//Напишите программу, которая упорядочит по возрастанию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//В итоге получается вот такой массив:
+//1 2 4 7
+//2 3 5 9
+//2 4 4 8
+
+
+Console.Clear();
+
+int[,] CreateMatrix(int row, int colum)
+{
+    int[,] matrix = new int[row, colum];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+
+}
+
+void PrintArray(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ArrangeAscending(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int minPos = j;
+            for (int k = j + 1; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, k] < matrix[i, minPos]) minPos = k;
+            }
+            int tmp = matrix[i, j];
+            matrix[i, j] = matrix[i, minPos];
+            matrix[i, minPos] = tmp;
+        }
+    }
+    return matrix;
+}
+Console.Write("Введи количество строк: ");
+int height = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи количество столбцов: ");
+int width = Convert.ToInt32(Console.ReadLine());
+PrintArray(ArrangeAscending(CreateMatrix(height, width)));
---
+++ Task4/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+массив размером 2 x 2 x 2
+12(0,0,0) 22(0,0,1)
+45(1,0,0) 53(1,0,1)*/
+
+Console.Clear();
+
+System.Console.WriteLine("Введите x ");
+int x = int.Parse(Console.ReadLine());
+System.Console.WriteLine(" Введите y :");
+int y = int.Parse(Console.ReadLine());
+System.Console.WriteLine(" Введите z :");
+int z = int.Parse(Console.ReadLine());
+int[,,] array = GetArray(x, y, z);
+int[,,] GetArray(int x,int y, int z)
+{
+    int[,,] array = new int[ x,  y,  z];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(10, 100);
+            }
+        }
+    }
+    return array;
+}
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i,j,k]}({i},{j},{k}) " );
+            }
+            Console.WriteLine();
+        }
+        
+    }
+     
+
+}
+
+PrintArray(array);
